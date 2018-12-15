6b6b6af8f841ef2d67ea67128de58dcce1f20f70
Angus Hollands <goosey15@gmail.com>
2018-12-15 19:17:48 +0000

    Automatically install hook inside Jupyter

--- numpy_html/__init__.py
+++ numpy_html/__init__.py
@@ -6,3 +6,11 @@ def install_jupyter_hook():
 
     html_formatter = get_ipython().display_formatter.formatters["text/html"]
     return html_formatter.for_type(np.ndarray, array_to_html)
+
+
+try:
+    ipython = get_ipython()
+except NameError:
+    pass
+else:
+    install_jupyter_hook()
