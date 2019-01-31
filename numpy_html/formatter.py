65ea4a1df8f5e3c4e886ce434b29ffc1213320c7
Angus Hollands <goosey15@gmail.com>
2019-01-31 14:19:21 +0000

    Code cleanups and annotations Black formatted `-l 120`

--- numpy_html/formatter.py
+++ numpy_html/formatter.py
@@ -1,58 +1,93 @@
-from .renderer import _render_table
-import numpy as np
+import typing
 from contextlib import contextmanager
+
+import numpy as np
+
+from .renderer import _render_table, ITEM_TYPE, INDEX_TYPE
+
+# Type of elements or templates
+ITEMS_TYPE = typing.Iterable[ITEM_TYPE]
 
 
 @contextmanager
-def printoptions(**opts):
+def print_options_as(**opts):
     old_options = np.get_printoptions()
     np.set_printoptions(**opts)
     yield
     np.set_printoptions(**old_options)
 
 
-def format_index(index):
+def format_index(index: INDEX_TYPE) -> typing.Union[INDEX_TYPE, int]:
+    """Format the index tuple corresponding to a particular array element. Return the
+
+    :param index: tuple of integers representing the array index
+    :return: sole entry of `index` if a tuple of length 1, otherwise `index`
+    """
     if len(index) == 1:
         return index[0]
     return index
 
 
-def format_items(items, format_element, **format_kwargs):
+def format_items(items: ITEMS_TYPE, format_element: typing.Callable[..., str], **format_kwargs) -> typing.Iterator[str]:
+    """Yield the formatted strings for the given items. Those items which are not templates are yielded directly.
+
+    :param items: iterable of templates or strings
+    :param format_element: array element formatter
+    :param format_kwargs: additional keyword arguments for element formatter
+    :return: Iterator of formatted strings
+    """
     for item in items:
         if isinstance(item, str):
             yield item
         else:
+            # We have a string template element, yield formatted string (using formatter and template)
             template, index, element = item
-            yield template.format(
-                format_index(index), format_element(element, **format_kwargs)
-            )
+            yield template.format(format_index(index), format_element(element, **format_kwargs))
 
 
-def fixed_format_element_npy(x, max_width=None):
+def fixed_format_element_npy(x, max_width: int = None) -> str:
+    """Fixed with formatter using numpy.array2string. If `max_width` is None, then return the formatted element
+    directly, otherwise left pad such that the final string has length `max_width`.
+
+    :param x: element to render
+    :param max_width: width of maximum element (predetermined)
+    :return: formatted string
+    """
     # Use numpy to format element
     x_str = np.array2string(x)
     if max_width is None:
         return x_str
-    
+
     # Return padded left-aligned string
     return f"{x_str:{max_width}}"
 
 
-def fixed_format_items(items):
+def fixed_format_items(items: ITEMS_TYPE) -> typing.List[str]:
+    """Format items using a fixed with formatter.
+
+    :param items: iterable of templates or strings
+    :return: formatted string
+    """
     items = list(items)
-    with printoptions(floatmode="maxprec"):
-        max_width = max(
-            len(fixed_format_element_npy(e))
-            for (_, _, e) in (t for t in items if isinstance(t, tuple))
-        )
+    with print_options_as(floatmode="maxprec"):
+        max_width = max(len(fixed_format_element_npy(e)) for (_, _, e) in (t for t in items if isinstance(t, tuple)))
         return [*format_items(items, fixed_format_element_npy, max_width=max_width)]
 
 
-def array_to_html(array, formatter=fixed_format_items, **formatter_kwargs):
+def array_to_html(
+    array: np.ndarray, formatter: typing.Callable[..., typing.List[str]] = fixed_format_items, **formatter_kwargs
+) -> str:
+    """Render NumPy array as an HTML table.
+
+    :param array: ndarray object
+    :param formatter: items formatter
+    :param formatter_kwargs: keyword arguments for items formatter
+    :return: HTML string
+    """
     print_options = np.get_printoptions()
     edge_items = print_options["edgeitems"]
     threshold = print_options["threshold"]
-    
+
     if array.size < threshold:
         edge_items = 0
 
--- numpy_html/renderer.py
+++ numpy_html/renderer.py
@@ -1,26 +1,40 @@
-import numpy as np
+import typing
 
+import numpy as np
 
 ELLIPSIS_STR_HORIZONTAL = "<td><center>⋯</center></td>"
 ELLIPSIS_STR_VERTICAL = "<td><center>⋮</center></td>"
-ELLPISIS_STR_DIAGONAL = "<td><center>⋱</center></td>"
+ELLIPSIS_STR_DIAGONAL = "<td><center>⋱</center></td>"
+
+TEMPLATE_TYPE = typing.Tuple[str, typing.Tuple[int, ...], typing.Any]
+ITEM_TYPE = typing.Union[TEMPLATE_TYPE, str]
+INDEX_TYPE = typing.Tuple[int, ...]
+ITEM_GENERATOR_TYPE = typing.Iterator[ITEM_TYPE]
+SUMMARY_RENDERER_TYPE = typing.Callable[[int, np.ndarray, int], typing.Iterator[str]]
+ITEM_RENDERER_TYPE = typing.Callable[[INDEX_TYPE, np.ndarray, int], ITEM_GENERATOR_TYPE]
 
 
-def ellipsis_renderer(ellipsis):
-    def wrapper(index: int, array: np.ndindex, edge_items: int):
-        yield ellipsis
+def make_constant_renderer(const: str) -> SUMMARY_RENDERER_TYPE:
+    """Factory function for a single ellipsis renderer.
+
+    :param const: constant string
+    :return: generator which produces string
+    """
+
+    def wrapper(index: INDEX_TYPE, array: np.ndarray, edge_items: int) -> typing.Iterator[str]:
+        yield const
 
     return wrapper
 
 
-def ellipsis_renderer_2D(index: int, array: np.ndindex, edge_items: int):
+def ellipsis_renderer_2D(index: INDEX_TYPE, array: np.ndarray, edge_items: int) -> typing.Iterator[str]:
     n, m = array.shape
     yield "<tr>"
     if m > 2 * edge_items:
         for i in range(edge_items):
             yield ELLIPSIS_STR_VERTICAL
 
-        yield ELLPISIS_STR_DIAGONAL
+        yield ELLIPSIS_STR_DIAGONAL
 
         for i in range(edge_items):
             yield ELLIPSIS_STR_VERTICAL
@@ -30,95 +44,97 @@ def ellipsis_renderer_2D(index: int, array: np.ndindex, edge_items: int):
     yield "</tr>"
 
 
-def extend_index(head, value):
-    return (*head, value)
+def extend_index(index: INDEX_TYPE, coordinate: int) -> INDEX_TYPE:
+    return (*index, coordinate)
 
 
 def render_array_summarized(
-    render_item, render_summary, index: tuple, array: np.ndarray, edge_items: int
-):
-    for i, item in enumerate(array[:edge_items]):
-        yield from render_item(extend_index(index, i), item, edge_items)
+    item_renderer: ITEM_RENDERER_TYPE,
+    summary_renderer: ITEM_RENDERER_TYPE,
+    index: INDEX_TYPE,
+    array: np.ndarray,
+    edge_items: int,
+) -> ITEM_GENERATOR_TYPE:
+    """Render array, summarising the inner items that have indices between `edge_items` and `len(array)-edge_items`.
 
-    yield from render_summary(index, array, edge_items)
+    :param item_renderer: item renderer
+    :param summary_renderer: summary item renderer
+    :param index: index
+    :param array: array to render
+    :param edge_items: number of edge items when summarising
+    :return:
+    """
+    for i, item in enumerate(array[:edge_items]):
+        yield from item_renderer(extend_index(index, i), item, edge_items)
+
+    yield from summary_renderer(index, array, edge_items)
 
     for i, item in enumerate(array[-edge_items:], start=len(array) - edge_items):
-        yield from render_item(extend_index(index, i), item, edge_items)
+        yield from item_renderer(extend_index(index, i), item, edge_items)
 
 
 def _render_array_generic(
-    render_item, render_summary, index: tuple, array: np.ndarray, edge_items: int
-):
+    item_renderer: ITEM_RENDERER_TYPE,
+    summary_renderer: ITEM_RENDERER_TYPE,
+    index: INDEX_TYPE,
+    array: np.ndarray,
+    edge_items: int,
+) -> ITEM_GENERATOR_TYPE:
+    """Render array, dispatching to `render_array_summarised` if required.
+
+    :param item_renderer: item renderer
+    :param summary_renderer: summary item renderer
+    :param index: index
+    :param array: array to render
+    :param edge_items: number of edge items when summarising
+    :return:
+    """
     if edge_items and len(array) > 2 * edge_items:
-        yield from render_array_summarized(
-            render_item, render_summary, index, array, edge_items
-        )
+        yield from render_array_summarized(item_renderer, summary_renderer, index, array, edge_items)
     else:
         for i, item in enumerate(array):
-            yield from render_item(extend_index(index, i), item, edge_items)
+            yield from item_renderer(extend_index(index, i), item, edge_items)
 
 
-def _render_row_1D(index: tuple, row, edge_items: int):
-    yield (
-        "<tr><td style='font-family:monospace;white-space: pre;' title='{}'>{}</td></tr>",
-        index,
-        row,
-    )
+def _render_row_1D(index: INDEX_TYPE, row, edge_items: int) -> ITEM_GENERATOR_TYPE:
+    yield ("<tr><td style='font-family:monospace;white-space: pre;' title='{}'>{}</td></tr>", index, row)
 
 
-def _render_array_1D(index: tuple, array: np.ndarray, edge_items: int):
+def _render_array_1D(index: INDEX_TYPE, array: np.ndarray, edge_items: int) -> ITEM_GENERATOR_TYPE:
     return _render_array_generic(
-        _render_row_1D,
-        ellipsis_renderer(f"<tr>{ELLIPSIS_STR_VERTICAL}</tr>"),
-        index,
-        array,
-        edge_items,
+        _render_row_1D, make_constant_renderer(f"<tr>{ELLIPSIS_STR_VERTICAL}</tr>"), index, array, edge_items
     )
 
 
-def _render_elem_2D(index: tuple, item, edge_items: int):
-    yield (
-        "<td style='font-family:monospace;white-space: pre;' title='{}'>{}</td>",
-        index,
-        item,
-    )
+def _render_elem_2D(index: INDEX_TYPE, item, edge_items: int) -> ITEM_GENERATOR_TYPE:
+    yield ("<td style='font-family:monospace;white-space: pre;' title='{}'>{}</td>", index, item)
 
 
-def _render_row_2D(index: tuple, row: np.ndarray, edge_items: int):
+def _render_row_2D(index: INDEX_TYPE, row: np.ndarray, edge_items: int) -> ITEM_GENERATOR_TYPE:
     yield "<tr>"
     yield from _render_array_generic(
-        _render_elem_2D,
-        ellipsis_renderer(ELLIPSIS_STR_HORIZONTAL),
-        index,
-        row,
-        edge_items,
+        _render_elem_2D, make_constant_renderer(ELLIPSIS_STR_HORIZONTAL), index, row, edge_items
     )
     yield "</tr>"
 
 
-def _render_array_2D(index: tuple, array: np.ndarray, edge_items: int):
-    yield from _render_array_generic(
-        _render_row_2D, ellipsis_renderer_2D, index, array, edge_items
-    )
+def _render_array_2D(index: INDEX_TYPE, array: np.ndarray, edge_items: int) -> ITEM_GENERATOR_TYPE:
+    yield from _render_array_generic(_render_row_2D, ellipsis_renderer_2D, index, array, edge_items)
 
 
-def _render_row_ND(index: tuple, row: np.ndarray, edge_items: int):
+def _render_row_ND(index: INDEX_TYPE, row: np.ndarray, edge_items: int) -> ITEM_GENERATOR_TYPE:
     yield "<tr><td>"
     yield from _render_table(index, row, edge_items)
     yield "</td></tr>"
 
 
-def _render_array_ND(index: tuple, array: np.ndarray, edge_items: int):
+def _render_array_ND(index: INDEX_TYPE, array: np.ndarray, edge_items: int) -> ITEM_GENERATOR_TYPE:
     yield from _render_array_generic(
-        _render_row_ND,
-        ellipsis_renderer(ELLIPSIS_STR_VERTICAL),
-        index,
-        array,
-        edge_items,
+        _render_row_ND, make_constant_renderer(ELLIPSIS_STR_VERTICAL), index, array, edge_items
     )
 
 
-def _render_array(index: int, array: np.ndarray, edge_items: int):
+def _render_array(index: INDEX_TYPE, array: np.ndarray, edge_items: int) -> ITEM_GENERATOR_TYPE:
     if len(array.shape) == 1:
         renderer = _render_array_1D
     elif len(array.shape) == 2:
@@ -128,7 +144,7 @@ def _render_array(index: int, array: np.ndarray, edge_items: int):
     return renderer(index, array, edge_items)
 
 
-def _render_table(index: tuple, array, edge_items: int):
+def _render_table(index: INDEX_TYPE, array, edge_items: int) -> ITEM_GENERATOR_TYPE:
     yield "<table style='border-style:solid;border-width:1px;'>"
     yield from _render_array(index, array, edge_items)
     yield "</table>"
