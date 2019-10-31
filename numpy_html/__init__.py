3da680c5234932eb46abaead24438381c8dffab9
Angus Hollands <goosey15@gmail.com>
2019-10-31 10:10:21 +0000

    Add `cls` arg to `install_jupyter_hook` to make it easier to support new libraries

--- numpy_html/__init__.py
+++ numpy_html/__init__.py
@@ -1,11 +1,15 @@
 from .formatter import array_to_html
+import numpy as np
 
 
-def install_jupyter_hook():
-    import numpy as np
+def install_jupyter_hook(cls=np.ndarray):
+    """Install Jupyter display hook for a given array class
 
+    :param cls: numpy-like array which is compatible with np.printoptions and np.array2string
+    :return:
+    """
     html_formatter = get_ipython().display_formatter.formatters["text/html"]
-    return html_formatter.for_type(np.ndarray, array_to_html)
+    return html_formatter.for_type(cls, array_to_html)
 
 
 try:
